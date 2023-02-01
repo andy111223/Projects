53f4997c6ab8c47383208250cb24f8dd97d56fb2
Andrzej Rzeszutek <andrzej.rzeszutek@gmail.com>
2023-02-01 18:49:48 +0100

    Add 2nd new line

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,5 +6,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hellold!");
         System.out.println("this is new line");
+        System.out.println("this is second line");
     }
 }
